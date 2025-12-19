27270c9bbf76a0c80a623326dacda9f966fb0238
Yoonus Anees <yoonusaneesniis@gmail.com>
2025-12-19 21:57:09 +0530

    Fixed(TopBar)

--- src/layouts/Topbar.jsx
+++ src/layouts/Topbar.jsx
@@ -4,13 +4,15 @@ import { useAuth } from '../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
 
 export default function Topbar() {
-  const { logout } = useAuth();
+  const { logout , user } = useAuth();
   const nav = useNavigate();
 
   return (
     <header className="sticky top-0 z-10 border-b border-zinc-200 bg-white/80 backdrop-blur">
-      <div className="flex items-center justify-between px-5 py-3">
+      <div className="flex items-center justify-center px-5 py-3">
         <div className="text-sm text-zinc-600">Hotel POS</div>
+        <div className="flex-1 ml-10">Hi {user?.username}</div>
+       
         <Button
           variant="ghost"
           onClick={() => {
